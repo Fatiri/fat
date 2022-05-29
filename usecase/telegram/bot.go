c6d8cdce2e7da761bfa5c28d0f38e4408f61d433
lhamfatiri <ilham.fatiri@thelionparcel.com>
2022-05-29 15:17:39 +0700

    add validation nil message comand

--- usecase/telegram/bot.go
+++ usecase/telegram/bot.go
@@ -39,6 +39,13 @@ func (tc *TelegramCtx) Bot(messages []string) {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
+		if update.Message == nil { // ignore any non-Message updates
+			continue
+		}
+
+		if !update.Message.IsCommand() { // ignore any non-command Messages
+			continue
+		}
 
 		// Create a new MessageConfig. We don't have text yet,
 		// so we leave it empty.
